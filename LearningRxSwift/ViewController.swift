a5a43da79c29fe751a8a3e9a7f124078f34a8015
jules <jules.papa@kakaocorp.com>
2018-02-07 10:17:32 +0900

    initial commit

---
+++ LearningRxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearningRxSwift
+//
+//  Created by jules.papa on 07/02/2018.
+//  Copyright © 2018 jules.papa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'LearningRxSwift' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for LearningRxSwift
+
+  target 'LearningRxSwiftTests' do
+    inherit! :search_paths
+    pod 'RxSwift', '~> 4.0'
+  end
+
+  target 'LearningRxSwiftUITests' do
+    inherit! :search_paths
+    pod 'RxSwift', '~> 4.0'
+  end
+
+end
